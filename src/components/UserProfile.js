d553fa33c0e91428027c576d82fa6e6721d137bd
andyols <andyolson09@gmail.com>
2019-03-21 22:09:03 -0500

    changed user profile to fontawesome icon

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -4,12 +4,9 @@ const UserProfile = () => {
   return (
     <div className="UserProfile">
       <div className="User">
-        <div className="name">Jack Oliver</div>
-        <div className="image">
-          <img
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/profile/profile-512_1.jpg"
-            alt="profile"
-          />
+        <div className="name">
+          Jack Oliver
+          <i className="far fa-user avatar" />
         </div>
       </div>
     </div>
