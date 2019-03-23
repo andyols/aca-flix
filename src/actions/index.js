9e0f24669fd7393a399fea28545d76f641c3a6e3
andyols <andyolson09@gmail.com>
2019-03-23 10:23:42 -0500

    initial action setup

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,52 @@
 import axios from 'axios'
+import { API_KEY } from '../config/keys'
+import {
+  LOAD_MY_MOVIE_LIST,
+  MY_MOVIE_LIST_LOADED,
+  SEARCH_RESULTS_LOADED
+} from './types'
 
 export const loadMyMovieList = () => dispatch => {
-  // make call to /movies
-  // dispatch to myMovieListLoaded
+  axios
+    .get('/movies')
+    .then(res =>
+      dispatch(myMovieListLoaded(res.data)).catch(err => console.log(err))
+    )
+    .catch(err => console.log(err))
+}
+
+export const myMovieListLoaded = movies => {
+  return {
+    type: MY_MOVIE_LIST_LOADED,
+    payload: movies
+  }
+}
+
+export const loadSearch = searchTerm => dispatch => {
+  axios
+    .get(
+      `https://api.themoviedb.org/3/search/multi?query=${searchTerm}&api_key=${API_KEY}`
+    )
+    .then(res => dispatch(searchLoaded(res.data.results)))
+    .catch(err => console.log(err))
+}
+
+export const searchLoaded = movies => {
+  return {
+    type: SEARCH_RESULTS_LOADED,
+    payload: movies
+  }
+}
+
+export const saveMyMovie = movie => dispatch => {
+  axios
+    .post('/movies', movie)
+    .then(res => dispatch(loadMyMovieList()))
+    .catch(err => console.log(err))
+}
+
+export const removeMyMovie = id => dispatch => {
+  axios
+    .delete(`/movies/${id}`)
+    .then(res => dispatch(loadMyMovieList()).catch(err => console.log(err)))
 }
